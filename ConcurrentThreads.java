844c731808634efc4732db0a90fb68d962069d7f
girish-gabra <girish.gabra10@gmail.com>
2016-05-28 02:17:38 -0400

    Added Collection for individual threads
    Added collection to store values produced by each individual thread.

--- ConcurrentThreads.java
+++ ConcurrentThreads.java
@@ -1,22 +1,36 @@
 package multithreading;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class ConcurrentThreads implements Runnable {
 	static int count=0;
 	
 	public void run()
 	{
-		while(true)
-		{
-			/* synchronized block to ensure only one thread at a time executes the following statements*/	
-			synchronized(this){
-				if(count<100){
-					count++;
-					System.out.println(count);
-				}else{
-					break;
-				}
-			}	
-		}
+		List<Integer> collection=new ArrayList<Integer>();
+			while(true)
+			{
+				/* synchronized block to ensure only one thread at a time executes the following statements*/	
+				synchronized(this){
+					if(count<100){
+						count++;
+						collection.add(count);
+						System.out.println(count);
+					}else{
+						display(collection);
+						break;
+					}
+				}	
+			}
+		
+	}
+	
+	public void display(List<Integer> collection)
+	{
+		System.out.println(Thread.currentThread().getName()+" "+collection);
 	}
 
 }
