30e7a2871fb4464b385dadfa2e9fdb9d28eb21ac
girish-gabra <girish.gabra10@gmail.com>
2016-05-28 03:38:28 -0400

    Modified for joining main thread
    Modified so that main exits at the end.

--- Main.java
+++ Main.java
@@ -4,15 +4,27 @@ public class Main {
 
 	public static void main(String[] args)
 	{
-		ConcurrentThreads cr = new ConcurrentThreads();
-		Thread a = new Thread(cr,"A");
-		Thread b = new Thread(cr,"B");
-		Thread c = new Thread(cr,"C");
 		
-		a.start();
-		b.start();
-		c.start();
-		
+		try 
+		{
+			ConcurrentThreads cr = new ConcurrentThreads();
+			Thread a = new Thread(cr,"A");
+			Thread b = new Thread(cr,"B");
+			Thread c = new Thread(cr,"C");
+			
+			a.start();
+			b.start();
+			c.start();
+			
+			a.join();
+			b.join();
+			c.join();
+		}catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
 		
 	}
 
